3fbec5d9c9c87e34c9f366e1a3e7707df6e7f036
Daniel <84340437+danielctds@users.noreply.github.com>
2023-08-29 17:36:44 +0200

    v 00.00

--- finderivatives/__init__.py
+++ finderivatives/__init__.py
@@ -1,2 +1,4 @@
 
-from derivative import Derivative
+from finderivatives.european_call import Call
+from finderivatives.european_put import Put
+from finderivatives.portfolio import Portfolio
--- finderivatives/derivative.py
+++ finderivatives/derivative.py
@@ -1,100 +1,146 @@
+"""
 
+pendiente:
+    incluir validaciones
+    ajustar put
+    
 
-import pandas as pd
-import math as mt
-import numpy as np
-import random as rd
-from scipy.stats import norm
+"""
 
+from finderivatives import validations as val
 
 #%%
-class Derivative():
+class EuropeanOption():
     
-    def __init__(self, strike, maturity, position):
-        self._strike = strike
-        self._maturity = maturity
-        self._position = position
-        self._flows = {}
-        
+    def __init__(self, strike, maturity, position, premium=0):
+        self._strike = val.validate_strike(strike)
+        self._maturity = val.validate_maturity(maturity)
+        self._dt = val.validate_maturity(maturity)
+        self._position = val.validate_position(position)
+        self._premium = val.validate_premium(premium)
+        # self._flows = {}
+    
+    
+    def __str__(self):
+        return 'xxxxx'
+    
+    
+    def __add__(self, other):
+        payoff = self.payoff() + other.payoff()
+        profit = self.profit() + other.profit()
+        pricing_bs = self.pricing_bs() + other.pricing_bs()
+        addition = {
+            'payoff': payoff,
+            'profit': profit,
+            'pricing_bs': pricing_bs
+            }
+        return addition
+    
+    
     def get_strike(self):
         return self._strike
-
-
-#%% Call
-class Call(Derivative):
-    
-    def __init__(self, strike, maturity, position):
-        super().__init__(strike, maturity, position)
-        
-    
-    def payoff(self, spot, premium=0):
-        
-        if self._position == 1:
-            self._payoff = max([0, spot - self._strike]) - premium
-        
-        elif self._position == -1:
-            self._payoff = min([0, self._strike - spot]) + premium
-        
-        return self._payoff
-        
-        
     
     
-    def valuation_bs(self, spot, vol, rf, b, t):
-        d1 = (mt.log(spot/self._strike) + (rf - b - (vol**2)/2)/t) \
-            / (vol * mt.sqrt(t))
-        d2 = d1 - vol * (t)**(1/2)
-        
-        v = spot * np.exp(-t) * norm.cdf(d1)\
-            - self._strike * np.exp(-rf) * norm.cdf(d2)
-        
-        return v
+    def get_maturity(self):
+        return self._maturity
+    
+    
+    def get_position(self):
+        return self._position
+    
+    
+    def set_spot(self, spot):
+        self._spot = val.validate_spot(spot)
+    
+    
+    def set_volatility(self, vol):
+        self._vol = vol
+    
+    
+    def set_r(self, r):
+        self._r = r
+    
+    
+    def set_market_inputs(self, spot, vol, r):
+        self.set_spot(spot)
+        self.set_volatility(vol)
+        self.set_r(r)
     
 
 
-#%%  Put
-class Put(Derivative):
-    
-    def __init__(self, strike, maturity, position):
-        super().__init__(strike, maturity, position)
-        
-    
-    def payoff(self, spot, premium=0):
-        
-        if self._position == 1:
-            self._payoff = max([0, self._strike - spot]) - premium
-        
-        elif self._position == -1:
-            self._payoff = min([0, spot - self._strike]) + premium
-        
-        return self._payoff
-
-    
-# browniano
-mu = 0.05
-sigma = 0.02
-s_0 = 100
-dt = 1/12
-s_1 = s_0 * mt.exp((mu - (sigma**2)*0.5)*dt \
-                   + sigma * mt.sqrt(dt) * rd.normalvariate(0,1))
-
-print(s_1)
-
-
-
-
-
+#%% Ejecucion directa
 if __name__ == '__main__':
-    print(' Ejecucion directa ...')
+    print(' Ejecucion directa ... \n')
     
-    derivado = Derivative(strike=100, maturity=2, position=1)
-    print(derivado.get_strike())
+    # strike = 105
+    # maturity = 2
+    # position = 1
+    # spot = 102
+    # spots = list(range(90, 110, 1))
+    # # spots = np.arange(strike*0.8, strike*1.2, strike*0.01)
+    # premium = 3
+    # vol = 0.05
+    # r = 0.01
+    # dt = 2
     
-    put = Put(strike=150, maturity=3, position=1)
-    print(put.get_strike())
+    
+    # # call = Call(strike=strike, maturity=maturity, position=position,
+    #             # premium=premium)
+    # call = Put(strike=strike, maturity=maturity, position=position,
+    #           premium=premium)
+    
+    # call.set_spot(spot)
+    # call_payoff1 = call.payoff()
+    # print('payoff:', call_payoff1)
+    
+    # call.set_spot(np.array(spot))
+    # call_payoff2 = call.payoff()
+    # print('payoff:', call_payoff2)
+    
+    # call.set_spot(spots)
+    # call_payoff3 = call.payoff()
+    # print('payoffs:', call_payoff3)
+    
+    # call.set_spot(spot)
+    # call_profit1 = call.profit()
+    # print('profit:', call_profit1)
+    
+    # call.set_spot(np.array(spot))
+    # call_profit2 = call.profit()
+    # print('profits:', call_profit2)
+    
+    # call.set_spot(spots)
+    # call_profit3 = call.profit()
+    # print('profits:', call_profit3)
 
-    call = Call(strike=180, maturity=4, position=1)
-    print(call.get_strike())
+    # call.set_market_inputs(spot, vol, r)
+    # call_pricing1 = call.pricing_bs()
+    # print('pricing:', call_pricing1)
     
-    call_valuation = call.valuation_bs(180, 0.05, 0.01, 0, 1)
-    print(call_valuation)
+    # call.set_market_inputs(np.array(spot), vol, r)
+    # call_pricing2 = call.pricing_bs()
+    # print('pricings:', call_pricing2)
+    
+    # call.set_market_inputs(spots, vol, r)
+    # call_pricing3 = call.pricing_bs()
+    # print('pricings:', call_pricing3)
+    
+    
+    
+    # import matplotlib.pyplot as plt
+    
+    # fig, ax = plt.subplots()
+    # ax.plot(spots, call_payoff3)
+    # ax.plot(spots, call_pricing3)
+    # plt.show()
+    
+    # fig, ax = plt.subplots()
+    # ax.plot(spots, put.pricing_bs(spot=spots, vol=vol, r=r, dt=dt))
+    # ax.plot(spots, put.payoff(spot=spots))
+    # plt.show()
+    
+    
+    # s = np.array([96, 98, 100, 102, 104])
+    # print('pricing:', call.payoff(spot=s))
+
+
