9da755ec53e1af87eec715c9f49674faf8f6b680
jaime4segura <jaime@MacBook-Pro-de-Jaime.local>
2023-12-07 17:23:54 +0100

    update to strategy.py, include multiple strategies

--- finderivatives/strategies.py
+++ finderivatives/strategies.py
@@ -64,7 +64,7 @@ class BearSpreadCall(Portfolio):
                             premium=premium1)
         self._call02 = Call(strike=strike2,
                             maturity=maturity,
-                            position=-1,
+                            position=1,
                             premium=premium2)
         super().__init__(self._call01, self._call02)
         
@@ -84,44 +84,147 @@ class BearSpreadPut(Portfolio):
         super().__init__(self._put01, self._put02)
 
 
-#%% Butterfly
+#%% Butterfly Spread
 
-class ButterflySpread(Portfolio):
+#### Butterfly Spread Call
+    ###Precio de compra: X - a y X + a - Precio de venta: X 
+
+class ButterflySpreadCall(Portfolio):
     
-    def __init__(self, *derivatives):
-        super().__init__(*derivatives)
+    def __init__(self, strike1, strike2, strike3, strike4, maturity, premium1 = 0,
+                 premium2 = 0, premium3 = 0, premium4 = 0):
+        
+        self._call01 = Call(strike=strike1,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium1)
+        self._call02 = Call(strike=strike2,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium2)
+        self._call03 = Call(strike=strike3,
+                            maturity=maturity,
+                            position=-1,
+                            premium=premium3)
+        self._call04 = Call(strike=strike4,
+                            maturity=maturity,
+                            position=-1,
+                            premium=premium4)
+        super().__init__(self._call01, self._call02, self._call03, self._call04)
+        
+    
+
+#### Butterfly Spread Put
+class ButterflySpreadPut(Portfolio):
+    
+    def __init__(self, strike1, strike2, strike3, strike4, maturity, premium1 = 0,
+                 premium2 = 0, premium3 = 0, premium4 = 0):
+        
+        self._put01 = Put(strike=strike1,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium1)
+        self._put02 = Put(strike=strike2,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium2)
+        self._put03 = Put(strike=strike3,
+                            maturity=maturity,
+                            position=-1,
+                            premium=premium3)
+        self._put04 = Put(strike=strike4,
+                            maturity=maturity,
+                            position=-1,
+                            premium=premium4)
+        super().__init__(self._put01, self._put02, self._put03, self._put04)
 
 
 #%% Straddle
-
+    #### Precios de strike iguales
+    
 class StraddleSpread(Portfolio):
     
-    def __init__(self, *derivatives):
-        super().__init__(*derivatives)
+    def __init__(self, strike1, strike2, maturity, premium1 = 0, premium2 = 0):
+        self._call01 = Call(strike=strike1,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium1)
+        
+        self._put01 = Put(strike=strike2,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium2)
+        
+        super().__init__(self._call01, self._put01)
 
 
 #%% Strip
+    #### Precios de strike iguales
 
 class StripSpread(Portfolio):
     
-    def __init__(self, *derivatives):
-        super().__init__(*derivatives)
+    def __init__(self, strike1, strike2, strike3, maturity, premium1 = 0, 
+                 premium2 = 0, premium3 = 0):
+        
+        self._call01 = Call(strike=strike1,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium1)
+        
+        self._put01 = Put(strike=strike2,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium2)
+        
+        self._put02 = Put(strike=strike3,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium3)
+        super().__init__(self._call01, self._put01, self._put02)
 
 
 #%% Strap
+    #### Precios de strike iguales
 
 class StrapSpread(Portfolio):
     
-    def __init__(self, *derivatives):
-        super().__init__(*derivatives)
-
+    def __init__(self, strike1, strike2, strike3, maturity, premium1 = 0, 
+                 premium2 = 0, premium3 = 0):
+        
+        self._call01 = Call(strike=strike1,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium1)
+        
+        self._call02 = Call(strike=strike2,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium2)
+        
+        self._put01 = Put(strike=strike3,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium3)
+        super().__init__(self._call01, self._call02, self._put01)
 
 #%% Strangle
-
+    #### Precios de strike put menor a call
+    
 class StrangleSpread(Portfolio):
     
-    def __init__(self, *derivatives):
-        super().__init__(*derivatives)
+    def __init__(self, strike1, strike2, maturity, premium1 = 0, premium2 = 0):
+        
+        self._call01 = Call(strike=strike1,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium1)
+        
+        self._put01 = Put(strike=strike2,
+                            maturity=maturity,
+                            position=1,
+                            premium=premium2)
+        
+        super().__init__(self._call01, self._put01)
 
 
 
