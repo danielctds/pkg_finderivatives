b6e5db1bde0a6d18d4d90836492b2532d267fcb4
Daniel <84340437+danielctds@users.noreply.github.com>
2024-03-05 22:39:18 +0100

    ReadTheDocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,8 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
+import sphinx_rtd_theme
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
